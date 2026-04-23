df64084d738a5cab9fa76a6096928ff5a0e462fb
vladislava-iva <vladislava.ivanova109@gmail.com>
2026-04-23 13:57:26 +0300

    Delete t5-web directory

--- t5-web/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$user = 'u82419';
-$pass = '7111555';
-$db = new PDO(
-    'mysql:host=localhost;dbname=u82419;charset=utf8mb4', $user, $pass,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
